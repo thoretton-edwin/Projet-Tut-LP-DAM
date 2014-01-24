bb38ae8295670ca1a9612417e0aaabe4f305506b
mpaloulack <mpaloulackgmail.com>
2014-01-24 09:50:26 +0100

    Projet vide
    Création du projet IOS

---
+++ IUTUnice/IUTUnice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IUTUnice
+//
+//  Created by Wazany on 24/01/2014.
+//  Copyright (c) 2014 UNS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IUTUnice/IUTUnice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IUTUnice
+//
+//  Created by Wazany on 24/01/2014.
+//  Copyright (c) 2014 UNS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
