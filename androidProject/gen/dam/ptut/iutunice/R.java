22aeef5b5969f46c4e3a0f52666075d282c683fd
NicolasScherer <sch71@hotmail.fr>
2014-01-24 14:11:48 +0100

    add activity second + list
    -> ajout d'une seconde vue correspondant au menu en liste

--- androidProject/gen/dam/ptut/iutunice/R.java
+++ androidProject/gen/dam/ptut/iutunice/R.java
@@ -24,10 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int imgMenuItem=0x7f080001;
+        public static final int listViewMenu=0x7f080000;
+        public static final int txtMenuItem=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_second=0x7f030001;
+        public static final int item_list_menu=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
