b2018b66c9f28340a424f316063250f793cf2fc8
CSemmezies <cindy.semmezies@gmail.com>
2014-02-07 18:22:19 +0100

    ActionBar
    Création de l'action bar

---
+++ androidProject/src/dam/ptut/iutunice/IconMenuFragment.java
@@ -0,0 +1,19 @@
+package dam.ptut.iutunice;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IconMenuFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View menu = inflater.inflate(R.layout.icon_menu_fragment, container, false);
+		
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
--- androidProject/src/dam/ptut/iutunice/MainActivity.java
+++ androidProject/src/dam/ptut/iutunice/MainActivity.java
@@ -1,22 +1,56 @@
 package dam.ptut.iutunice;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		this.setTitle("IUT Nice Côte d'Azur");
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		
+		return super.onCreateOptionsMenu(menu);
+
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+            openSettings();
+			return true;
+		case R.id.action_list:
+			switchListMenu();
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+		
+	}
+
+	private void switchListMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void openSettings() {
+		// TODO Auto-generated method stub
+//		Intent intent = new Intent(MainActivity.this,
+//				ClassName.class);
+//		startActivity(intent);
+	}
+	
+
 
 }
