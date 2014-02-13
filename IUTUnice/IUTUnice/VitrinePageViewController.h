9aa1f29996e364ef6a8850fb2d4494170cb43916
giyomm <GiyommStarsky@gmail.com>
2014-02-13 14:22:40 +0100

    Vitrine Page update
    Add image Slideshow
    Add ScrollView and its text content
    Add « sondage » and « formations » buttons

--- IUTUnice/IUTUnice/VitrinePageViewController.h
+++ IUTUnice/IUTUnice/VitrinePageViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface VitrinePageViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *presentationImage;
+@property (weak, nonatomic) IBOutlet UIScrollView *descriptionScrollView;
+@property (weak, nonatomic) IBOutlet UIButton *sondageButton;
+@property (weak, nonatomic) IBOutlet UIButton *formationButton;
 
 @end
--- IUTUnice/IUTUnice/VitrinePageViewController.m
+++ IUTUnice/IUTUnice/VitrinePageViewController.m
@@ -18,7 +18,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        self.title = @"Vitrine";
+        self.title = @"Bienvenue à l'IUT";
     }
     return self;
 }
@@ -26,7 +26,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    _presentationImage.animationImages = [NSArray arrayWithObjects:
+                                          [UIImage imageNamed:@"IUT NICE.JPG"],
+                                          [UIImage imageNamed:@"IUT SOPHIA.JPG"],
+                                          [UIImage imageNamed:@"IUT MENTON195.JPG"],
+                                          [UIImage imageNamed:@"IUT NICE2.JPG"],
+                                          [UIImage imageNamed:@"menton.jpg"],nil];
+    
+    _presentationImage.animationDuration = 20.00;
+    _presentationImage.animationRepeatCount = 0; //infinite
+    _presentationImage.contentMode = UIViewContentModeScaleAspectFit;
+    [_presentationImage startAnimating];
+    
+    NSString *myPath = [[NSBundle mainBundle]pathForResource:@"descriptionIUT" ofType:@"txt"];
+    
+    NSString *presentationText = [[NSString alloc]initWithContentsOfFile:myPath encoding:NSUTF8StringEncoding error:nil];
+    
+    UITextView *myUITextView = [[UITextView alloc] initWithFrame:CGRectMake(0,0,_descriptionScrollView.frame.size.width,_descriptionScrollView.frame.size.height)];
+    myUITextView.text = presentationText;
+    myUITextView.textColor = [UIColor lightGrayColor];
+    myUITextView.font = [UIFont systemFontOfSize:14];
+    [myUITextView setBackgroundColor:[UIColor clearColor]];
+    myUITextView.editable = NO;
+    myUITextView.scrollEnabled = YES;
+    [_descriptionScrollView addSubview:myUITextView];
 }
 
 - (void)didReceiveMemoryWarning
