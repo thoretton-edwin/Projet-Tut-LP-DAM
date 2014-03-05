b60a30ba0ded357a911a27cab2097e79eaa02584
NicolasScherer <sch71@hotmail.fr>
2014-03-05 16:43:05 +0100

    Page des paramètres (fragments) -> gestion du retour arrière sur les versions Android inférieurs à 4.1 -> affichage liste par fragment -> ajouts d'icônes

--- androidProject/gen/dam/ptut/iutunice/R.java
+++ androidProject/gen/dam/ptut/iutunice/R.java
@@ -22,25 +22,32 @@ public final class R {
     }
     public static final class drawable {
         public static final int app_background=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_action_settings=0x7f020002;
-        public static final int ic_action_view_as_list=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int logo_addressbook=0x7f020005;
-        public static final int logo_bg=0x7f020006;
-        public static final int logo_calendar=0x7f020007;
-        public static final int logo_compagny_partnership=0x7f020008;
-        public static final int logo_grades=0x7f020009;
-        public static final int logo_iut_window=0x7f02000a;
-        public static final int logo_map=0x7f02000b;
-        public static final int logo_maps=0x7f02000c;
-        public static final int logo_post_bac=0x7f02000d;
-        public static final int logo_suaps=0x7f02000e;
-        public static final int logo_twitter=0x7f02000f;
+        public static final int apropos=0x7f020001;
+        public static final int arrow=0x7f020002;
+        public static final int connexion=0x7f020003;
+        public static final int credit=0x7f020004;
+        public static final int flag=0x7f020005;
+        public static final int ic_action_search=0x7f020006;
+        public static final int ic_action_settings=0x7f020007;
+        public static final int ic_action_view_as_list=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int logo_addressbook=0x7f02000a;
+        public static final int logo_bg=0x7f02000b;
+        public static final int logo_calendar=0x7f02000c;
+        public static final int logo_compagny_partnership=0x7f02000d;
+        public static final int logo_grades=0x7f02000e;
+        public static final int logo_iut_window=0x7f02000f;
+        public static final int logo_map=0x7f020010;
+        public static final int logo_maps=0x7f020011;
+        public static final int logo_post_bac=0x7f020012;
+        public static final int logo_suaps=0x7f020013;
+        public static final int logo_twitter=0x7f020014;
+        public static final int sondage=0x7f020015;
+        public static final int wifi=0x7f020016;
     }
     public static final class id {
-        public static final int action_list=0x7f08001d;
-        public static final int action_settings=0x7f08001c;
+        public static final int action_list=0x7f08001e;
+        public static final int action_settings=0x7f08001d;
         public static final int flContent=0x7f080000;
         public static final int horizontalScrollView1=0x7f080010;
         public static final int imgBtCalendar=0x7f080013;
@@ -58,25 +65,26 @@ public final class R {
         public static final int imgGrades=0x7f080008;
         public static final int imgIutWindow=0x7f080007;
         public static final int imgMap=0x7f08000a;
-        public static final int imgMenuItem=0x7f08001a;
+        public static final int imgParameterArrow=0x7f08001c;
+        public static final int imgParameterItem=0x7f08001a;
         public static final int imgPostBac=0x7f08000c;
         public static final int imgSuaps=0x7f08000b;
         public static final int imgTwitter=0x7f08000f;
-        public static final int listViewMenu=0x7f080002;
-        public static final int listViewParameter=0x7f080001;
+        public static final int listViewParameter=0x7f080002;
         public static final int scrollView1=0x7f080003;
         public static final int textView1=0x7f080004;
         public static final int tvSuapsActuality=0x7f080005;
         public static final int tvTweetsActuality=0x7f080006;
-        public static final int txtMenuItem=0x7f08001b;
+        public static final int txtParameterItem=0x7f08001b;
+        public static final int vgContent=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_parameter=0x7f030001;
-        public static final int activity_second=0x7f030002;
+        public static final int fragment_item_parameter=0x7f030002;
         public static final int icon_menu_fragment=0x7f030003;
         public static final int icon_menu_liste_fragment=0x7f030004;
-        public static final int item_list_menu=0x7f030005;
+        public static final int item_list_parameter=0x7f030005;
     }
     public static final class menu {
         public static final int icon_menu=0x7f070000;
---
+++ androidProject/src/dam/ptut/iutunice/App.java
@@ -0,0 +1,9 @@
+package dam.ptut.iutunice;
+
+import java.util.List;
+
+import android.app.Application;
+
+public class App extends Application {
+	List<ParameterItem> parameterListe;
+}
