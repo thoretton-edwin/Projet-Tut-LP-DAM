b5a50454259932f09075dc9a6c13e5bf94bbebfd
NicolasScherer <sch71@hotmail.fr>
2014-03-13 14:46:36 +0100

    Affichage de la liste des Wifi disponibles

--- androidProject/src/dam/ptut/iutunice/ParameterWifiFragment.java
+++ androidProject/src/dam/ptut/iutunice/ParameterWifiFragment.java
@@ -20,7 +20,10 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -38,16 +41,13 @@ public class ParameterWifiFragment extends Fragment {
 	boolean success = false;
 		
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	public View onCreateView(final LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		View content = inflater.inflate(R.layout.fragment_wifi, container,
 				false);
 		
-		recoveryXmlWifi(content);
-		if(success){
-			//affichage de la liste
-		}
+		recoveryXmlWifi(content, inflater);
 		
 		
 
@@ -75,13 +75,59 @@ public class ParameterWifiFragment extends Fragment {
 		
 		return content;
 	}
+	//- Méthode permettant d'afficher les informations de XmlWifi
+	private void showXmlWifi(final View content, final LayoutInflater inflater){
+		//affichage de la liste
+		ListView listView = (ListView) content.findViewById(R.id.listViewWifi);
+		listView.setAdapter(new BaseAdapter() {
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				View itemView = inflater.inflate(R.layout.item_list_wifi, parent,
+						false);
+				
+				TextView txtSite = (TextView) itemView.findViewById(R.id.txtSite);
+				TextView txtNameWifi = (TextView) itemView.findViewById(R.id.txtNameWifi);
+				TextView txtSsid = (TextView) itemView.findViewById(R.id.txtSsid);
+				TextView txtClef = (TextView) itemView.findViewById(R.id.txtClef);
+				TextView txtLogin = (TextView) itemView.findViewById(R.id.txtLogin);
+				TextView txtMdp = (TextView) itemView.findViewById(R.id.txtMdp);
+				
+				WifiItem wifiItem = getItem(position);
+				txtSite.setText(wifiItem.place);
+				txtNameWifi.setText(wifiItem.name);
+				txtSsid.setText("SSID : "+wifiItem.ssid);
+				txtClef.setText("Clef : "+wifiItem.key);
+				txtLogin.setText("Login : "+wifiItem.login);
+				txtMdp.setText("Mot de passe : "+ wifiItem.password);
+				
+				return itemView;
+			}
+
+			@Override
+			public long getItemId(int position) {
+				return 0;
+			}
+
+			@Override
+			public WifiItem getItem(int position) {
+				return listWifi.get(position);
+			}
+
+			@Override
+			public int getCount() {
+				return listWifi.size();
+			}
+		});
+	}
 	//- Méthode permettant de récupérer le xml Wifi
-	private void recoveryXmlWifi(final View content){
+	private void recoveryXmlWifi(final View content, final LayoutInflater inflater){
 		client.get(getActivity(), "http://sd-6.archive-host.com/membres/up/4041201007a5bf3f0d5112ca7648a0adc66e3177/IUT_Nice_Cote_dAzur/infosWifi.xml", 
 		new TextHttpResponseHandler() {
 			@Override
 			public void onFinish() {
-		
+				if(success){
+					showXmlWifi(content, inflater);
+				}
 			}
 
 			@Override
@@ -89,7 +135,6 @@ public class ParameterWifiFragment extends Fragment {
 				try {
 				//parse XML
 				parseWifi(responseBody);
-				success = true;
 				} catch (XmlPullParserException e) {
 					e.printStackTrace();
 					Toast.makeText(getActivity(), "Erreur de lecture des données", Toast.LENGTH_LONG).show();
@@ -97,16 +142,21 @@ public class ParameterWifiFragment extends Fragment {
 					e.printStackTrace();
 					Toast.makeText(getActivity(), "Erreur de connexion lors de la lecture des données", Toast.LENGTH_LONG).show();
 				}
+				success = true;
 				//barre de progression
 				ProgressBar bar = (ProgressBar) content.findViewById(R.id.progressBarWifi);
 				//rend invisible la barre de chargement
 				bar.setVisibility(ProgressBar.GONE);
+				TextView txtLoadingWifi = (TextView) content.findViewById(R.id.txtLoadingWifi);
+				txtLoadingWifi.setText("");
 				
 			}
 
 			@Override
 			public void onFailure(String responseBody, Throwable error) {
-				Toast.makeText(getActivity(), "Erreur de connexion", Toast.LENGTH_LONG).show();
+				//Toast.makeText(getActivity(), "Erreur de connexion", Toast.LENGTH_LONG).show();
+				TextView txtLoadingWifi = (TextView) content.findViewById(R.id.txtLoadingWifi);
+				txtLoadingWifi.setText("Erreur de connexion !");
 			}
 		});
 	}
