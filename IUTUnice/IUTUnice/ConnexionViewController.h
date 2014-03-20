c560e88535952fc93ef3a4cd4fc51b6f4308a5e5
mpaloulack <mpaloulackgmail.com>
2014-03-20 14:54:43 +0100

    Add pages in  setting

---
+++ IUTUnice/IUTUnice/ConnexionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConnexionViewController.h
+//  IUTUnice
+//
+//  Created by Wazany on 20/03/2014.
+//  Copyright (c) 2014 UNS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConnexionViewController : UIViewController
+
+@end
--- IUTUnice/IUTUnice/SettingsViewController.m
+++ IUTUnice/IUTUnice/SettingsViewController.m
@@ -26,6 +26,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.title=@"Settings";
+    
 
     tabSettings= [[NSMutableArray alloc] init];
     [tabSettings addObject:@"Connexion"];
@@ -34,6 +36,8 @@
     [tabSettings addObject:@"Sondage"];
     [tabSettings addObject:@"Langue"];
     [tabSettings addObject:@"Credits"];
+    
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
 }
 
 - (void)didReceiveMemoryWarning
@@ -53,16 +57,37 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return 6;
+
+    int number = tabSettings.count;
+   
+    
+    return number;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    int number = tabSettings.count;
+     NSLog(@"nombre de row : %d", number);
+    if (indexPath.row <= number) {
+        return 50;
+    }else
+    {
+        return 0;
+    }
+    
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    tableView.scrollEnabled = NO;
     NSString *CellIdentifier = @"Cell1";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
     if (cell == Nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.textLabel.text =[tabSettings objectAtIndex:indexPath.row];
+        cell.selectionStyle = UITableViewCellSelectionStyleBlue;
+        cell.accessoryType = UITableViewCellAccessoryNone;
     }
     
    NSLog(@"%@",[tabSettings objectAtIndex:indexPath.row]);
@@ -70,55 +95,71 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a story board-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    NSLog(@"row:%d",[[tableView indexPathForSelectedRow] row]);
+    [self choixMenu:[[tableView indexPathForSelectedRow] row]];
+    
 }
 
- */
+-(void) choixMenu:(int) tag
+{
+    switch(tag)
+    {
+        case 0: // Connexion
+        {
+            ConnexionViewController *viewController = [[ConnexionViewController alloc] initWithNibName:@"ConnexionViewController" bundle:nil];
+            [self.navigationController pushViewController:viewController animated:YES];
+
+			
+        }
+            break;
+        case 1: // Infos Wifi
+        {
+            infosWifiViewController *viewController = [[infosWifiViewController alloc] initWithNibName:@"infosWifiViewController" bundle:nil];
+            [self.navigationController pushViewController:viewController animated:YES];
+			
+        }
+            break;
+        case 2: // A propos
+        {
+            AboutViewController *viewController = [[AboutViewController alloc] initWithNibName:@"AboutViewController" bundle:nil];
+            [self.navigationController pushViewController:viewController animated:YES];
+			
+        }
+            break;
+        case 3: // Sondage
+        {
+            SurveyViewController *viewController = [[SurveyViewController alloc] initWithNibName:@"SurveyViewController" bundle:nil];
+            [self.navigationController pushViewController:viewController animated:YES];
+			
+        }
+            break;
+        case 4: // Langue
+        {
+            LangueViewController *viewController = [[LangueViewController alloc] initWithNibName:@"LangueViewController" bundle:nil];
+            [self.navigationController pushViewController:viewController animated:YES];
+
+			
+        }
+            break;
+        case 5: // Crédit
+        {
+			
+            CreditsViewController *viewController = [[CreditsViewController alloc] initWithNibName:@"CreditsViewController" bundle:nil];
+            [self.navigationController pushViewController:viewController animated:YES];
+			
+        }
+        
+            break;
+        default:
+            // lancer une exception ? ..
+            break;
+            
+    }
+    
+}
+
 
 @end
