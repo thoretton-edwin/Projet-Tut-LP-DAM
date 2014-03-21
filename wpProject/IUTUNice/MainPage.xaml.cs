bb842a935db138d687830b162fcc10d1b0fd0f65
TheGreatOzymandias <alan.cassisa@gmail.com>
2014-03-21 11:19:11 +0100

    Commit Matin
    Youhou

--- wpProject/IUTUNice/MainPage.xaml.cs
+++ wpProject/IUTUNice/MainPage.xaml.cs
@@ -17,7 +17,7 @@ namespace IUTUNice
         public MainPage()
         {
             InitializeComponent();
-            webView1.Navigate(new Uri("https://twitter.com/Univ_Nice"), null, /*"User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows Phone OS 7.0; Trident/3.1; IEMobile/7.0; HTC; T8788)"*/ "");
+            //webView1.Navigate(new Uri("https://twitter.com/Univ_Nice"), null, /*"User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows Phone OS 7.0; Trident/3.1; IEMobile/7.0; HTC; T8788)"*/ "");
 
             // Exemple de code pour la localisation d'ApplicationBar
             //BuildLocalizedApplicationBar();
