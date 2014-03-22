a818ba39aeb7d2d713ce6abab2ce9ee61d9289b4
thoretton <thoretton.edwin@gmail.com>
2014-03-22 11:15:54 +0100

    MAJ annuaire

--- IUTUnice/IUTUnice/AnnuaireViewController.h
+++ IUTUnice/IUTUnice/AnnuaireViewController.h
@@ -2,13 +2,12 @@
 //  AnnuaireViewController.h
 //  IUTUnice
 //
-//  Created by Wazany on 14/02/2014.
+//  Created by Edwin Thoretton on 21/03/2014.
 //  Copyright (c) 2014 UNS. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import "FormationListViewController.h"
 
-@interface AnnuaireViewController : UITableViewController
+@interface AnnuaireViewController : UIViewController
 
 @end
