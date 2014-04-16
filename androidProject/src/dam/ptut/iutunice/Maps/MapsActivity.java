826c1f3f477425e05910dd9fc0d482de4a41f8ad
PapilusNook <papilusnook@gmail.com>
2014-04-16 10:43:44 +0200

    Ajout de la map

--- androidProject/src/dam/ptut/iutunice/Maps/MapsActivity.java
+++ androidProject/src/dam/ptut/iutunice/Maps/MapsActivity.java
@@ -14,6 +14,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -30,11 +32,11 @@ public class MapsActivity extends FragmentActivity {
 				.findFragmentById(R.id.map)).getMap();
 		
 		// Coordonnées (latitute et longitude) des différents sites de l'IUT
-		LatLng iutSophia = new LatLng(43.616984, 7.071000);
-		LatLng iutCannes = new LatLng(43.550314, 7.000636);
-		LatLng iutCannesBocca = new LatLng(43.553305, 6.967531);
-		LatLng iutMenton = new LatLng(43.777035, 7.501133);
-		LatLng iutNice = new LatLng(43.687409, 7.227308);
+		final LatLng iutSophia = new LatLng(43.616984, 7.071000);
+		final LatLng iutCannes = new LatLng(43.550314, 7.000636);
+		final LatLng iutCannesBocca = new LatLng(43.553305, 6.967531);
+		final LatLng iutMenton = new LatLng(43.777035, 7.501133);
+		final LatLng iutNice = new LatLng(43.687409, 7.227308);
 		
 		int hue = 0;
 		map.addMarker(new MarkerOptions().title("iut Sophia")
@@ -54,6 +56,60 @@ public class MapsActivity extends FragmentActivity {
 				.position(iutNice));
 		
 		map.moveCamera(CameraUpdateFactory.newLatLngZoom(iutSophia, 15));
+		
+		// Clic sur le bouton Nice 
+		Button button = (Button) findViewById(R.id.btnMapsNice);
+		button.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View v) {
+		        // Do something in response to button click
+		    	map.moveCamera(CameraUpdateFactory.newLatLngZoom(iutSophia, 15));
+		    }
+		});
+		
+		// Clic sur le bouton Nice 
+		Button btnNice = (Button) findViewById(R.id.btnMapsNice);
+		btnNice.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				 // Do something in response to button click
+				 map.moveCamera(CameraUpdateFactory.newLatLngZoom(iutNice, 15));
+			}
+		});
+				
+		// Clic sur le bouton Menton
+		Button btnMenton = (Button) findViewById(R.id.btnMapsMenton);
+		btnMenton.setOnClickListener(new View.OnClickListener() {
+		public void onClick(View v) {
+				// Do something in response to button click
+				map.moveCamera(CameraUpdateFactory.newLatLngZoom(iutMenton, 15));
+			}
+		});
+		
+		// Clic sur le bouton Cannes La Bocca
+		Button btnCannesLB = (Button) findViewById(R.id.btnMapsCannesLB);
+		btnCannesLB.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Do something in response to button click
+				map.moveCamera(CameraUpdateFactory.newLatLngZoom(iutCannesBocca, 15));
+			}
+		});
+		
+		// Clic sur le bouton Cannes
+		Button btnCannes = (Button) findViewById(R.id.btnMapsCannes);
+		btnCannes.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+				// Do something in response to button click
+				map.moveCamera(CameraUpdateFactory.newLatLngZoom(iutCannes, 15));
+			}
+		});
+		
+		// Clic sur le bouton Sophia 
+		Button btnSophia = (Button) findViewById(R.id.btnMapsSophia);
+		btnSophia.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Do something in response to button click
+				map.moveCamera(CameraUpdateFactory.newLatLngZoom(iutSophia, 15));
+			}
+		});
 	}
 
 	@Override
