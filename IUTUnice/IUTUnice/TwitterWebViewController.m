3ff524d7f33677365cc8463809ebccc72cbc3128
thoretton <thoretton.edwin@gmail.com>
2014-05-07 11:37:14 +0200

    MAJ twitter

--- IUTUnice/IUTUnice/TwitterWebViewController.m
+++ IUTUnice/IUTUnice/TwitterWebViewController.m
@@ -33,9 +33,12 @@
     [super viewDidLoad];
 	self.title=@"Twitter UNS";
 	//init
+	int barHeight = self.navigationController.navigationBar.frame.size.height+20;
+	int webViewHeight =[UIScreen mainScreen].bounds.size.height-barHeight;
+	
 	webView =
 	[[UIWebView alloc]initWithFrame:
-	 CGRectMake(0,0,[UIScreen mainScreen].bounds.size.width,[UIScreen mainScreen].bounds.size.height)];
+	 CGRectMake(0,barHeight,[UIScreen mainScreen].bounds.size.width,webViewHeight)];
 	 
 	[self.webView setDelegate:self];
 	 
@@ -48,29 +51,30 @@
 	
     activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];    
 		
-	[self.view addSubview:webView];
+	
+	int width = activityIndicator.frame.size.width;
+	NSLog(@"width: %d",width);
+	activityIndicator.frame = CGRectMake(([UIScreen mainScreen].bounds.size.width-width)/2,150,width,width);
+    activityIndicator.color = [UIColor blueColor];
+    [self.view addSubview:activityIndicator];
+	
 	
 }
 
 -(void)webViewDidStartLoad:(UIWebView *)webView
 {
 	NSLog(@"start web view");
-
-    activityIndicator.frame = CGRectMake(50,50, [UIScreen mainScreen].bounds.size.width/2, [UIScreen mainScreen].bounds.size.height/2);
-    activityIndicator.color = [UIColor blueColor];
-    [self.view addSubview:activityIndicator];
-    
     [activityIndicator startAnimating];
 
     
     
 }
 
--(void )webViewDidFinishLoad:(UIWebView *)webView
+-(void )webViewDidFinishLoad:(UIWebView *)_webView
 {
 	NSLog(@"end load web view");
-    
 	[activityIndicator stopAnimating];
+	[self.view addSubview:webView];
 }
 
 - (void)didReceiveMemoryWarning
